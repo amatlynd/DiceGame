fb033575a17ecf61811ffdffa024e9d340029ed8
Lyndon Amat <lyndon.amat@mail.utoronto.ca>
2021-03-27 21:59:08 -0400

    Javascript for header as well as dice
    Added javascript to page so that upon refresh there is a new pair of dice as well as it telling who won

---
+++ index.js
@@ -0,0 +1,25 @@
+function generateDiceNumber() {
+  return Math.floor((Math.random()*6) + 1);
+}
+
+function changeDiceFace(imgName) {
+  var randomNumber = generateDiceNumber();
+  document.getElementsByClassName(imgName)[0].src = "images/dice" + randomNumber + ".png";
+  return randomNumber;
+}
+
+function updatePage(){
+  var randomNumber1 = changeDiceFace("img1");
+  var randomNumber2 = changeDiceFace("img2");
+
+  //Checks to see who wins and updates the page
+  if (randomNumber1 > randomNumber2) {
+    document.getElementsByTagName("h1")[0].innerText = " 🚩Player 1 Wins!";
+  } else if (randomNumber1 < randomNumber2) {
+    document.getElementsByTagName("h1")[0].innerText = "Player 2 Wins! 🚩";
+  } else {
+    document.getElementsByTagName("h1")[0].innerText = "Tied!";
+  }
+}
+
+updatePage();
